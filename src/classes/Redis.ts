123a4cacec34e229e9cc32be2ce1778626dc6c21
David Lopes <davidribeirolopes@gmail.com>
2017-03-26 22:46:17 -0300

    Adding the Messaging class, playing with redis

--- src/classes/Redis.ts
+++ src/classes/Redis.ts
@@ -1,2 +1,28 @@
-import * as BPromise from 'bluebird';
+
+import * as Redis from 'redis';
+require('dotenv').config();
+
+class Messaging{
+
+    public client: Redis.RedisClient;
+
+    constructor(){
+        this.client = Redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST);
+    }
+
+    quit(){
+        this.client.quit();
+    }
+
+    createQueue(){
+
+    }
+}
+
+
+const messaging = new Messaging();
+
+messaging.client.get('chave', Redis.print);
+
+
 
